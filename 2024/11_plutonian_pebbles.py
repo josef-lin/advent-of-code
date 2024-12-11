0a9411ae7dacb975fbcdb7bc82f04a93b31f4ce8
Joseph Lin <Joseph.Lin10@thg.com>
2024-12-11 10:01:30 +0000

    use default dict, dont use string for splitting left and right half

--- 2024/11_plutonian_pebbles.py
+++ 2024/11_plutonian_pebbles.py
@@ -1,28 +1,31 @@
 import time
+from collections import defaultdict
 start_time = time.time()
 
 def string_to_list(s):
     return [int(num) for num in s.split(' ')]
 
 def evolve(data):
-    # MAX_ITER = 25
+    MAX_ITER = 25
     # MAX_ITER = 75
-    MAX_ITER = 250
-    counter = {num:1 for num in data}
+    # MAX_ITER = 250
+    counter = defaultdict(int)
+    for num in data:
+        counter[num] += 1
     i = 0
     while i < MAX_ITER:
-        new_counter = {}
+        new_counter = defaultdict(int)
         for num, count in counter.items():
             if num == 0:
-                new_counter[1] = count + new_counter.get(1,0)
+                new_counter[1] += count 
             elif len(str(num)) % 2 == 0:
                 half = len(str(num)) // 2
-                left = int(str(num)[:half])
-                right = int(str(num)[half:]) 
-                new_counter[left] = count + new_counter.get(left,0)
-                new_counter[right] = count + new_counter.get(right,0)
+                left = num // (10**half)
+                right = num % (10**half)
+                new_counter[left] += count 
+                new_counter[right] += count
             else:
-                new_counter[2024*num] = count + new_counter.get(2024*num,0)
+                new_counter[2024*num] += count
         counter = new_counter
         i += 1
     return sum(counter[num] for num in counter)
